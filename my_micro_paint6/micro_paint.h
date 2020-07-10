5ba67a0712236ae51168a192936a748e46f5b02e
Mia-Cross <leila.marabese@gmail.com>
2020-07-10 02:29:21 +0200

    6eme version, normalement c'est mon dernier commit parce que l'exam c'est demain

---
+++ my_micro_paint6/micro_paint.h
@@ -0,0 +1,27 @@
+#ifndef MICRO_PAINT_H
+# define MICRO_PAINT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+
+typedef struct s_area
+{
+	int w;
+	int h;
+	char c;
+	char **buf;
+}			t_area;
+
+typedef struct s_rect
+{
+	char r;
+	float x;
+	float y;
+	float w;
+	float h;
+	char c;
+}			t_rect;
+
+#endif
